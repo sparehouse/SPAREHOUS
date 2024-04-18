eb573c33868322e3ab2987a80dc7c2dcfc805423
Ezooooo <ezooahmed39@gmail.com>
2024-04-18 21:38:22 +0200

    old

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,13 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 function authenticate(req, res, next) {
-  const authHeader = req.headers.authorization;
+  const token = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Authorization header missing or invalid' });
+  if (!token) {
+    return res.status(401).json({ message: 'Authorization header missing' });
   }
-
-  const token = authHeader.split(' ')[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
