8d9ee900e17c655b3875e54ca3581431559960f3
Ezooooo <ezooahmed39@gmail.com>
2024-04-18 18:21:01 +0200

    search by loc, filter for price and beds

--- controllers/apartmentController.js
+++ controllers/apartmentController.js
@@ -11,5 +11,38 @@ async function getAllApartments(req, res) {
     res.status(500).json({ message: 'Server error' });
   }
 }
+async function getApartmentsByLocation(req, res) {
+  try {
+    const { location } = req.query;
+    const apartments = await Apartment.find({ location: { $regex: new RegExp(location, 'i') } });
+    res.json(apartments);
+  } catch (error) {
+    console.error('Error fetching apartments by location:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+}
 
-module.exports = { getAllApartments };
+async function getApartmentsByBeds(req, res) {
+  try {
+    const { beds } = req.query;
+    const apartments = await Apartment.find({ beds: parseInt(beds) });
+    res.json(apartments);
+  } catch (error) {
+    console.error('Error fetching apartments by beds:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+}
+
+async function getApartmentsByPriceRange(req, res) {
+  try {
+    const { minPrice, maxPrice } = req.query;
+    const apartments = await Apartment.find({ price: { $gte: parseInt(minPrice), $lte: parseInt(maxPrice) } });
+    res.json(apartments);
+  } catch (error) {
+    console.error('Error fetching apartments by price range:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+}
+
+
+module.exports = { getAllApartments,getApartmentsByLocation, getApartmentsByBeds, getApartmentsByPriceRange };
--- routes/apartmentRoutes.js
+++ routes/apartmentRoutes.js
@@ -5,5 +5,8 @@ const router = express.Router();
 const apartmentController = require('../controllers/apartmentController');
 
 router.get('/apartments', apartmentController.getAllApartments);
+router.get('/apartments/location', apartmentController.getApartmentsByLocation);
+router.get('/apartments/beds', apartmentController.getApartmentsByBeds);
+router.get('/apartments/price', apartmentController.getApartmentsByPriceRange);
 
 module.exports = router;
