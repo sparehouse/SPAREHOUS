9e57e67f8fce1effd88f2700c38511af9707ad78
Ezooooo <ezooahmed39@gmail.com>
2024-04-19 02:59:23 +0200

    delete func

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,12 +59,12 @@ async function getUserDetails(req, res) {
   
 async function deleteAccount(req, res) {
     try {
-        // Check if userId exists in req.user
-        if (!req.user || !req.user.userId) {
+        // Check if userId exists in session
+        if (!req.session || !req.session.userId) {
             return res.status(401).json({ message: 'Unauthorized' });
         }
 
-        const userId = req.user.userId; // Extract user ID from request object
+        const userId = req.session.userId; // Extract user ID from session
         await User.findByIdAndDelete(userId);
         res.json({ message: 'Account deleted successfully' });
     } catch (error) {
@@ -72,6 +72,7 @@ async function deleteAccount(req, res) {
         res.status(500).json({ message: 'Server error' });
     }
 }
+
 
   
   module.exports = { getUserDetails, updateName, updatePassword, deleteAccount };
